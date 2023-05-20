7f74191735232648b2b8886cc01bc0b528af9b0d
JOÃO VICTOR BUENO RAMALHO <3280.343@edu.fb.org.br>
2023-05-19 21:53:22 -0300

    PRIMEIRO EXEMPLO DE COMANDOS SQL

---
+++ ms-sql/aula1.sql.sql
@@ -0,0 +1,10 @@
+USE AdventureWorks2012
+
+SELECT * FROM SYS.tables
+
+SELECT * FROM Person.Person 
+SELECT FirstName FROM Person.Person
+
+SELECT  FirstName AS PrimeiroNome,
+		LastName AS UltimoNome
+FROM Person.Person
