e08735cc9edf94bca878d42f5eb34edc4210d8fb
JOÃO VICTOR BUENO RAMALHO <3280.343@edu.fb.org.br>
2023-05-23 21:54:32 -0300

    novos comandos

--- ms-sql/aula3.sql
+++ ms-sql/aula3.sql
@@ -43,3 +43,41 @@ SELECT Distinct Name FROM Production.Product
 SELECT COUNT(TITLE) FROM PERSON.Person
 WHERE Title = 'Ms'
 SELECT DISTINCT TITLE FROM Person.Person
+
+SELECT ListPrice FROM Production.Product
+WHERE ListPrice >= 1000
+AND ListPrice <= 1500
+
+SELECT ListPrice FROM Production.Product
+WHERE ListPrice BETWeEN 1000 AND 1500
+
+SELECT COUNT(HireDate) as 'qnt de contratados em 1 ano' FROM HumanResources.Employee
+WHERE HireDate BETWEEN '2009/01/01' AND '2010/01/01'
+
+SELECT * FROM PERSON.PERSON 
+WHERE BusinessEntityID = 5
+OR BusinessEntityID = 111
+OR BusinessEntityID = 23
+
+
+SELECT * FROM Person.Person
+WHERE BusinessEntityID IN (5,111,23)
+
+SELECT * FROM Person.Person
+WHERE FirstName LIKE 'A%'
+
+--QUANTOS PRODUTOS TEMOS NO BD ACIMA DE 1500
+
+SELECT COUNT(ListPrice) FROM Production.Product
+WHERE ListPrice > 1500
+
+--QUANTAS PESSOAS TEM O SOBRENOME QUE COMEÇA COM A LETRA P
+
+SELECT COUNT(LastName) FROM Person.Person
+WHERE LastName LIKE 'P%'
+
+--EM QUANTAS CIDADES UNICAS ESTÃO OS CLIENTE
+
+SELECT * FROM Person.Address
+SELECT COUNT(DISTINCT City) FROM Person.Address
+
